414e1b281f9aea407a82c7985b59704c04a8c1e3
Lan Ejiri <lan.ejiri@yahoo.com>
2018-05-13 23:23:40 -0400

    shuffles cards

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import Card from "./components/card/card.js";
 import cards from "./cards.json";
 
-const App = () => <Card people={cards}/>;
+//this shuffles the object
+const randomCards = cards.sort(function(a,b){return 0.5 - Math.random()});
+
+//pass in the random array 
+const App = () => <Card people={randomCards}/>
 
 export default App;
--- clicky-game/src/components/card/card.js
+++ clicky-game/src/components/card/card.js
@@ -1,17 +1,10 @@
 import React from "react";
 import "./card.css";
 
-// const Card = ({ alt, name, image }) => (
-//   <div className="cardshape">
-//     <li> Name: {name}</li>
-//     <img className="cardimage" src={image} alt={alt} />
-//   </div>
-// );
-
 const Card = ({ people }) => {
   const peopleList = people.map(({ image, alt }) => (
     <div className="cardshape">
-    <img src={image} alt={alt} className="cardimage"/>;
+    <img src={image} alt={alt} className="cardimage"/>
     </div>
   ));
 
